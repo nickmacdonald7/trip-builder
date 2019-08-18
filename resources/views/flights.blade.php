c8a111cfd0896b9bbe2d38a3265f10902dbc0cea
Nick <nickmacdonald7@gmail.com>
2019-08-18 14:31:50 -0300

    conditional display of... any flights!

--- resources/views/flights.blade.php
+++ resources/views/flights.blade.php
@@ -48,44 +48,46 @@
                     <div class="form-group">
                         <form method="POST" action="">
                             @csrf
-                            <h1></h1>
-
-                            <h2>Choose a departing flight from {{ Session::get('departureAirport.city') }} on {{ date('F jS', strtotime(Session::get('departureDate'))) }}:</h2>
-                            <div class="form-input">
-                                <label>City</label>
-                                <select class="form-control" id="departureFlight" name="departureFlight">
-                                    @foreach ($departureFlights as $flight)
-                                        <option
-                                            id ="{{ $flight->id }}"
-                                            {{ old('departureFlight') == $flight->id ? 'selected' : '' }}
-                                            value="{{ $flight->id }}"
-                                        >
-                                            {{ $flight->code }}{{ $flight->number }} -
-                                            {{$flight->da_code}} to {{$flight->aa_code}}
-                                            (departing {{ date('H:i', strtotime($flight->departure_time)) }} - arriving {{ date('H:i', strtotime($flight->arrival_time)) }})
-                                        </option>
-                                    @endforeach
-                                </select>
-                            </div>
+                            @if (count($departureFlights) > 0)
+                                <h2>Choose a departing flight from {{ Session::get('departureAirport.city') }} on {{ date('F jS', strtotime(Session::get('departureDate'))) }}:</h2>
+                                <div class="form-input">
+                                    <label>City</label>
+                                    <select class="form-control" id="departureFlight" name="departureFlight">
+                                        @foreach ($departureFlights as $flight)
+                                            <option
+                                                id ="{{ $flight->id }}"
+                                                {{ old('departureFlight') == $flight->id ? 'selected' : '' }}
+                                                value="{{ $flight->id }}"
+                                            >
+                                                {{ $flight->code }}{{ $flight->number }} -
+                                                {{$flight->da_code}} to {{$flight->aa_code}}
+                                                (departing {{ date('H:i', strtotime($flight->departure_time)) }} - arriving {{ date('H:i', strtotime($flight->arrival_time)) }})
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            @else
+                                <h2>No flights to display. Try choosing another city!</h2>
+                            @endif
 
                             @if (count($returnFlights) > 0)
-                            <h2>Choose a return flight from {{ Session::get('arrivalAirport.city') }} on {{ date('F jS', strtotime(Session::get('returnDate'))) }}</h2>
-                            <div class="form-input">
-                                <label>City</label>
-                                <select class="form-control" id="returnFlight" name="returnFlight">
-                                    @foreach ($returnFlights as $flight)
-                                        <option
-                                            id ="{{ $flight->id }}"
-                                            {{ old('returnFlight') == $flight->id ? 'selected' : '' }}
-                                            value="{{ $flight->id }}"
-                                        >
-                                            {{ $flight->code }}{{ $flight->number }} -
-                                            {{$flight->da_code}} to {{$flight->aa_code}}
-                                            (departing {{ date('H:i', strtotime($flight->departure_time)) }} - arriving {{ date('H:i', strtotime($flight->arrival_time)) }})
-                                        </option>
-                                    @endforeach
-                                </select>
-                            </div>
+                                <h2>Choose a return flight from {{ Session::get('arrivalAirport.city') }} on {{ date('F jS', strtotime(Session::get('returnDate'))) }}</h2>
+                                <div class="form-input">
+                                    <label>City</label>
+                                    <select class="form-control" id="returnFlight" name="returnFlight">
+                                        @foreach ($returnFlights as $flight)
+                                            <option
+                                                id ="{{ $flight->id }}"
+                                                {{ old('returnFlight') == $flight->id ? 'selected' : '' }}
+                                                value="{{ $flight->id }}"
+                                            >
+                                                {{ $flight->code }}{{ $flight->number }} -
+                                                {{$flight->da_code}} to {{$flight->aa_code}}
+                                                (departing {{ date('H:i', strtotime($flight->departure_time)) }} - arriving {{ date('H:i', strtotime($flight->arrival_time)) }})
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                </div>
                             @endif
                             @if ($errors->any())
                                 <div class="alert alert-danger">
@@ -96,7 +98,9 @@
                                     </ul>
                                 </div>
                             @endif
-                            <button type="submit">Start Building</button>
+                            @if (count($departureFlights) > 0 && count($returnFlights) > 0)
+                                <button type="submit">Start Building</button>
+                            @endif
                         </form>
                     </div>
             </div>
