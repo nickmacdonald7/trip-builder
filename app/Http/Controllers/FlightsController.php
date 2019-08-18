bf325aa84c482f0f9b07c955626e558264abcb4b
Nick <nickmacdonald7@gmail.com>
2019-08-18 16:05:23 -0300

    wrong id! not airline, flight id!

--- app/Http/Controllers/FlightsController.php
+++ app/Http/Controllers/FlightsController.php
@@ -30,7 +30,7 @@ class FlightsController extends Controller
             ->join('airline', 'flight.airline_id', '=', 'airline.id')
             ->join('airport AS da', 'flight.departure_airport_id', '=', 'da.id')
             ->join('airport AS aa', 'flight.arrival_airport_id', '=', 'aa.id')
-            ->select('airline.id', 'airline.code', 'flight.number', 'flight.departure_time', 'flight.arrival_time', 'da.code AS da_code', 'aa.code AS aa_code')
+            ->select('airline.code', 'flight.id', 'flight.number', 'flight.departure_time', 'flight.arrival_time', 'da.code AS da_code', 'aa.code AS aa_code')
             ->where('flight.departure_airport_id', '=', $departureAirportId)
             ->where('flight.arrival_airport_id', '=', $arrivalAirportId)
             ->get();
